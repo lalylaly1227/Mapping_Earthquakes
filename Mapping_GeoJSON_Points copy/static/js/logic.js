28ae6069c4327ca3e102a951354b2e169797b0f8
Gladys Robles <105124485+lalylaly1227@users.noreply.github.com>
2022-08-18 18:18:30 -0400

    update for transfer

--- Mapping_GeoJSON_Points copy/static/js/logic.js
+++ Mapping_GeoJSON_Points copy/static/js/logic.js
@@ -4,8 +4,8 @@ console.log("working");
 // Create the map object with center and zoom level.
 let map = L.map('mapid').setView([30, 30], 2);
 
-// We create the tile layer that will be the background of our map.
-let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/streets-v11/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+// We create the dark view tile layer that will be an option for our map.
+let dark = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/dark-v10/tiles/{z}/{x}/{y}?access_token={accessToken}', {
     attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
     maxZoom: 18,
     accessToken: API_KEY
@@ -27,10 +27,4 @@ d3.json(airportData).then(function (data) {
             console.log(layer);
             layer.bindPopup("<h2> Airport code: " + feature.properties.faa + "</h2> <hr> <h3> Airport name: " + feature.properties.name + "</h3>");
         }
-    }).addTo(map)
-    // Creating a GeoJSON layer with the retrieved data.
-        //   L.geoJSON(data).addTo(map);
-    });
-
-
-
+    }).addTo(map);
